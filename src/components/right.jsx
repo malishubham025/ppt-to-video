82ff8700fede5ac836593649d49863ff98e80936
malishubham025 <malishubham025@gmail.com>
2024-03-10 12:29:34 +0530

    backend complete

--- src/components/right.jsx
+++ src/components/right.jsx
@@ -5,13 +5,14 @@ import Loader from "./loader";
 function FileUpload() {
     const [selectedFile, setSelectedFile] = useState(null);
     const [loading, setLoading] = useState(false);
-
+    var [name,setName]=React.useState("");
     const handleFileChange = (event) => {
         setSelectedFile(event.target.files[0]);
     };
 
     const handleUpload = () => {
         if (selectedFile) {
+            setName("File Uploading");
             setLoading(true); // Set loading to true when starting upload
             if (
                 selectedFile.type === 'application/vnd.ms-powerpoint' ||
@@ -25,13 +26,69 @@ function FileUpload() {
                     method: 'POST',
                     body: formData,
                 })
-                .then(response => {
-                    setLoading(false); // Set loading to false when upload completes
-                    if (response.ok) {
-                        alert('Upload successful!');
-                    } else {
-                        alert('Upload failed.');
+                .then(data => {
+                    // setLoading(false); // Set loading to false when upload completes
+                    if (!data.ok) {
+                        alert('Upload Failed!');
                     }
+                    else{
+                         return data.json();
+                    }
+                        
+                })
+                .then((data)=>{
+                    setName("File Uploading 50%");
+                    const images_presence = data.images_presence;
+                    const file_path=data.file_path;
+                    fetch('http://127.0.0.1:8000/image_presence', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json' // Set content type to JSON
+                        },
+                        body: JSON.stringify({  // Convert data to JSON format
+                            images_presence: images_presence,
+                            file_path: file_path
+                        })
+                    })                    
+                    .then((responce)=>{
+                        setName("File Uploading 100%");
+                        setLoading(false); 
+                        if (!responce.ok) {
+                            alert('processing  Failed!');
+                        }
+                        else{
+                            setName("File Uploading 80%");
+                            setLoading(true);
+                             return responce.json();
+                        }
+                    })
+                    .then((responce)=>{
+                        setName("uploading 100%");
+
+                        fetch('http://127.0.0.1:8000/image_explain', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json' // Set content type to JSON
+                        },
+                        body: JSON.stringify({  // Convert data to JSON format
+                            "explanations":responce.explanations,
+                            "images_presence":responce.images_presence,
+                            "Title":responce.Title,
+                            "file_paths":responce.file_paths
+                        })
+                        }).then((responce)=>{
+                            if(responce.ok){
+                                setName("uploading complete");
+                               
+                                setTimeout(()=>{
+                                    setLoading(false);
+                                },5000);
+                            }
+                            else{
+                                alert("error");
+                            }
+                        })   
+                    })
                 })
                 .catch(error => {
                     setLoading(false); // Set loading to false in case of error
@@ -44,11 +101,14 @@ function FileUpload() {
         } else {
             alert('Please select a file to upload.');
         }
+
+        
+        
     };
 
     return (
         <div>
-            {loading && <Loader  name="taking scrrenshots"/>} {/* Display loader when loading is true */}
+            {loading && <Loader  name={name}/>} {/* Display loader when loading is true */}
             <form className='uploader' onClick={() => { document.querySelector("#file").click(); }}>
                 <label htmlFor="file">Upload a ppt File</label>
                 <div style={{ width: "10%" }}>
